8839b10880ca257d585e6b0216fef7bc73dee586
rkjc <richardkjcross@gmail.com>
2021-05-05 10:53:54 -0700

    quick code update

--- FinalsPrep/src/finalspractice/StartZoo.java
+++ FinalsPrep/src/finalspractice/StartZoo.java
@@ -3,6 +3,7 @@ package finalspractice;
 public class StartZoo {
 
 	public static void main(String[] args) {
+		System.out.println();
 		// command line program
 		
 		// make 3 LandAnimal - fill in all their attributes
@@ -17,6 +18,7 @@ public class StartZoo {
 		// every time the user pushes enter display a description
 		// of the Exhibit and the animal that it contains.
 		// as you leave the loop thank the user for visiting the Zoo
+		System.out.println("Welcome to the Zoo. Enjoy your journey of discovery today as you visit the exhibits.");
 		
 	}
 
