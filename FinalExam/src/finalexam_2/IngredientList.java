7ab2d1dfbf0af42680ca7c994ca4f39964a10010
rkjc <richardkjcross@gmail.com>
2021-05-20 12:21:46 -0700

    quick code update

--- FinalExam/src/finalexam_2/IngredientList.java
+++ FinalExam/src/finalexam_2/IngredientList.java
@@ -17,13 +17,23 @@ public class IngredientList {
 		return ingredientList;
 	}
 	
+//	public double getTotalIngredientCost() {
+//		double totalCost = 0;	
+//		for (Ingredient stuff : ingredientList) {
+//			totalCost += stuff.getCostPerMeasurementAmmount() * stuff.getAmountUsed();
+//		}
+//		double totalRoundOff = (double) Math.round(totalCost * 100) / 100; // optional
+//		return totalRoundOff;
+//	}
+	
 	public double getTotalIngredientCost() {
 		double totalCost = 0;	
-		for (Ingredient stuff : ingredientList) {
-			totalCost += stuff.getCostPerMeasurementAmmount() * stuff.getAmountUsed();
+		for (int i = 0; i < ingredientList.size(); i++) {
+			totalCost += ingredientList.get(i).getCostPerMeasurementAmmount() * ingredientList.get(i).getAmountUsed();
 		}
 		double totalRoundOff = (double) Math.round(totalCost * 100) / 100; // optional
 		return totalRoundOff;
 	}
+	
 	
 }
